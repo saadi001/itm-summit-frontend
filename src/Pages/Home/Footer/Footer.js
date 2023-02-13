a425f907dda934fe26750748dbdfae4163ac1d17
Sheikh sadi <sheikhsadi1620@gmail.com>
2023-02-14 01:59:51 +0600

    navbar updated

--- src/Pages/Home/Footer/Footer.js
+++ src/Pages/Home/Footer/Footer.js
@@ -5,7 +5,7 @@ import { FaFacebook,FaYoutube,FaGithub } from "react-icons/fa";
 const Footer = () => {
      return (
           <div>
-               <footer className="bg-white mt-4 dark:bg-gray-900">
+               <footer className="bg-white mt-6 dark:bg-gray-900">
                     <div className="flex flex-col items-center justify-between py-6 mx-auto space-y-4 sm:space-y-0 sm:flex-row">
                          <a href="#home">
                               <img className="w-24" src={logo} alt=""/>
@@ -13,10 +13,10 @@ const Footer = () => {
 
                          <p className="text-sm text-center text-gray-600 dark:text-gray-300">© Copyright 2023. <br /> All Rights Reserved by <br /> Department of Information Technology & Management</p>
 
-                         <div className="flex gap-2">
-                              <a href=""><FaFacebook/></a>
-                              <a href=""><FaYoutube/></a>
-                              <a href=""><FaGithub/></a>
+                         <div className="flex gap-2 text-2xl">
+                              <a href="/"><FaFacebook/></a>
+                              <a href="/"><FaYoutube/></a>
+                              <a href="/"><FaGithub/></a>
                          </div>
                     </div>
                </footer>
--- src/Pages/Home/TopLandingPage/Navbar/Navbar.js
+++ src/Pages/Home/TopLandingPage/Navbar/Navbar.js
@@ -1,15 +1,29 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../../../Asset/logo/328496960_760463888831247_1725153779242283475_n-removebg-preview.png'
 
 const Navbar = () => {
+     const [color, setColor] = useState(false)
+
+     const changeColor = () =>{
+          if(window.scrollY >= 90){
+               setColor(true)
+          }
+          else{
+               setColor(false)
+          }
+     }
+
+     window.addEventListener('scroll', changeColor);
+
      return (
-          <div className='lg:mx-20 md:mx-12 mx-2 '>
-               <div className="navbar justify-between text-white">
+          <div className={`lg:px-20 md:px-12 px-2 py-[-16px] z-10 ${color ? 'bg-white/90 text-gray-800 fixed left-0 right-0 ' : 'bg-transparent text-white'} duration-200 ease-in`} >
+               <div className="navbar justify-between">
                     <div className="flex justify-between">
                          <div className="dropdown">
                               {/* mobile view  */}
                               <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                                   <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                                   <svg xmlns="http://www.w3.org/2000/svg" className={`h-6 w-6 ${color ? 'text-gray-800':'text-white'}`} fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                               </label>
                               <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box text-gray-800 w-52">
                                    <li><a href='#home' className='px-6 py-3'>Home</a></li>
@@ -19,14 +33,14 @@ const Navbar = () => {
                                              Speakers
                                              <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
                                         </a>
-                                        <ul className="p-2 bg-base-100">
+                                        <ul className="p-2 bg-base-100 z-20">
                                              <li><a href='#keynotespeaker'>Keynote Speaker</a></li>
                                              <li><a>Panel Member</a></li>
                                         </ul>
                                    </li>
                                    <li><a href='#schedule' className='px-6 py-3'>Schedule</a></li>
                                    <li><a href='#contact' className='px-6 py-3'>Contact</a></li>
-                                   <li className='bg-pink-700 px-6 py-3 rounded-md hover:bg-blue-800 duration-200 ease-in cursor-pointer'>Competition</li>
+                                   <li className={`bg-pink-700 px-6 py-3 rounded-md hover:bg-blue-800 duration-200 ease-in cursor-pointer ${color && 'text-white'}`}>Competition</li>
                               </ul>
                          </div>
                          <Link to={'/'} className="w-16 md:w-24 text-xl"><img src={logo} alt="" /></Link>
@@ -48,7 +62,7 @@ const Navbar = () => {
                               </li>
                               <li><a href='#schedule'>Schedule</a></li>
                               <li><a href='#contact'>Contact</a></li>
-                              <li className='bg-pink-700 px-6 py-3 rounded-md hover:bg-blue-800 duration-200 ease-in cursor-pointer'>Competition</li>
+                              <li className={`bg-pink-700 px-6 py-3 rounded-md hover:bg-blue-800 duration-200 ease-in cursor-pointer ${color && 'text-white'}`}>Competition</li>
                          </ul>
                     </div>
 
